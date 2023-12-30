b6c58ae24298feba182bdde752f80b1934d7f775
randallcandaso <110688328+randallcandaso@users.noreply.github.com>
2023-12-30 15:51:09 -0700

    Update friends.py

--- friends.py
+++ friends.py
@@ -1,19 +1,3 @@
-'''
-
-File: friends.py
-Author: Randall Candaso
-Course/Section: CSC 120-002
-Purpose: This program takes an inputted file and reads
-in the names in the file. Each name is used to create
-a node that is then inserted into a linked list. In
-addition, each name node will have a friends attribute
-that, itself, links to another linked list containing
-that name's friends. The user will input two names
-into the program and the list of name(s) in common
-will be returned.
-
-'''
-
 from linked_list import LinkedList
 from linked_list import Node
 
